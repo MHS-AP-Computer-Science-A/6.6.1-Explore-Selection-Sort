46687302483bdfed79100973858a01fa97a0041e
foxrw1 <78601276+foxrw1@users.noreply.github.com>
2025-12-23 14:30:02 -0500

    Refactor selection sort implementation

--- src/ExploreSelectionSort.java
+++ src/ExploreSelectionSort.java
@@ -8,18 +8,15 @@ public class ExploreSelectionSort {
     // Postcondition: Uses Selection Sort to put arr
     // in ascending order
     public static void selectionSortAscending(int[] arr) {
-        // Traverse indexes up to second to last element
-        for (int index = 0; index < arr.length - 1; index++) {
-            // Find the smallest value
-            int minIndex = index;
-            for (int i = index + 1; i < arr.length; i++) {
-                if (arr[i] < arr[minIndex]) {
-                    minIndex = i;
+        for (int i = 0; i < arr.length - 1; i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[j] < arr[minIndex]) {
+                    minIndex = j;
                 }
             }
-            // Swap minIndex and index
-            int temp = arr[index];
-            arr[index] = arr[minIndex];
+            int temp = arr[i];
+            arr[i] = arr[minIndex];
             arr[minIndex] = temp;
         }
     }
@@ -87,4 +84,5 @@ public class ExploreSelectionSort {
     }
 
 }
+
 
